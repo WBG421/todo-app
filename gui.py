48f141932ec4d958171ced5908c62d7dc3f8a36a
Wojciech Bury <lokart421@gmail.com>
2023-01-08 18:12:31 +0100

    Create gui.py and rename main to cli

---
+++ gui.py
@@ -0,0 +1,9 @@
+import PySimpleGUI as sg
+
+label = sg.Text("Type in to-do")
+input_box = sg.InputText(tooltip="Enter to do")
+add_button = sg.Button("Add")
+
+window = sg.Window("To-Do App", layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
