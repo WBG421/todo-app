cbbabea2cdd60b4dd9180721b13d5e2dc9cde070
Wojciech Bury <lokart421@gmail.com>
2023-01-12 17:15:51 +0100

    Implement add new todo

--- web.py
+++ web.py
@@ -1,4 +1,24 @@
 import streamlit as st
+from helpers import open_todos, write_todos
 
-st.title("Hello")
-st.write("You can use this app to increase productivity")
+def main():
+    todos = open_todos()
+
+    st.title("Hello")
+    st.write("You can use this app to increase productivity")
+
+    for todo in todos:
+        st.checkbox(todo)
+
+    st.text_input(label="Add todo", label_visibility="hidden",
+                  placeholder="Add new todo...",
+                  on_change=add_todo, key="new_todo")
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos = open_todos()
+    todos.append(todo)
+    write_todos(todos)
+
+if __name__ == "__main__":
+    main()
