95c996c7210d24f553e861b3870940f49f3547f0
Wojciech Bury <lokart421@gmail.com>
2023-01-12 17:46:32 +0100

    Implement complete in web.py

--- web.py
+++ web.py
@@ -8,11 +8,18 @@ def main():
     st.write("You can use this app to increase productivity")
 
     for todo in todos:
-        st.checkbox(todo)
+        checkbox = st.checkbox(todo, key=todo)
+        if checkbox:
+            todos.remove(todo)
+            write_todos(todos)
+            del st.session_state[todo]
+            st._rerun()
 
     st.text_input(label="Add todo", label_visibility="hidden",
                   placeholder="Add new todo...",
                   on_change=add_todo, key="new_todo")
+
+    st.session_state
 
 def add_todo():
     todo = st.session_state["new_todo"]
